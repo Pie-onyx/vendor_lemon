4eaeb9b71ca5cab9badcaf2c95caf7e83412dba1
jhenrique09 <jhenrique09.mcz@hotmail.com>
2017-08-28 16:22:55 -0300

    prebuilt: Bring APNs from LOS

--- config.mk
+++ config.mk
@@ -17,6 +17,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     vendor/aosp/prebuilt/media/bootanimation.zip:system/media/bootanimation.zip
 
+# World APN list
+PRODUCT_COPY_FILES += \
+    vendor/aosp/prebuilt/etc/apns-conf.xml:system/etc/apns-conf.xml
+
 # Allow tethering without provisioning app
 PRODUCT_PROPERTY_OVERRIDES += \
     net.tethering.noprovisioning=true
