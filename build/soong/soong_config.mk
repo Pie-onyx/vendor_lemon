edf339bcbdeaaa9cf5a55ea2250c4e13e123a193
Stricted <info@stricted.net>
2018-02-17 17:28:52 +0100

    soong_config: Replace space with colon on TARGET_LD_SHIM_LIBS
    that way we can split the shim list logically into multiple lines like
    
    TARGET_LD_SHIM_LIBS := /system/bin/gpsd|/vendor/lib/libshim_gpsd.so \
        /system/lib/libexynoscamera.so|/vendor/lib/libexynoscamera_shim.so \
        /system/lib/libstagefright.so|/system/lib/libstagefright_shim.so
    
    to not have one single huge line
    
    [lineage-17.0]
    this got lost in:
        soong: rework soong_config for Pie
        Change-Id: I3c5001f02975e5487ba66a02217f1d24d2021272
    
    Change-Id: Ice1411ee22b78da1dbf22eac412b9e4978acebdf

--- build/soong/soong_config.mk
+++ build/soong/soong_config.mk
@@ -8,6 +8,6 @@ $(call add_json_bool, Has_legacy_camera_hal1, $(filter true,$(TARGET_HAS_LEGACY_
 $(call add_json_bool, Needs_text_relocations, $(filter true,$(TARGET_NEEDS_PLATFORM_TEXT_RELOCATIONS)))
 $(call add_json_str_omitempty, Target_init_vendor_lib, $(TARGET_INIT_VENDOR_LIB))
 $(call add_json_str_omitempty, Target_process_sdk_version_override, $(TARGET_PROCESS_SDK_VERSION_OVERRIDE))
-$(call add_json_str_omitempty, Target_shim_libs, $(TARGET_LD_SHIM_LIBS))
+$(call add_json_str_omitempty, Target_shim_libs, $(subst $(space),:,$(TARGET_LD_SHIM_LIBS)))
 
 $(call end_json_map)
