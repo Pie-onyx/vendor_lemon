bf83265bf6930929b3ec0dc8fbaf5b1e4ba99d23
Simon Shields <simon@lineageos.org>
2018-08-10 14:40:27 +1000

    soong: Rework soong_config for Pie
    Change-Id: I3c5001f02975e5487ba66a02217f1d24d2021272

--- build/soong/soong_config.mk
+++ build/soong/soong_config.mk
@@ -1,10 +1,10 @@
-# Insert new variables inside the Lineage structure
-lineage_soong:
-	$(hide) mkdir -p $(dir $@)
-	$(hide) (\
-	echo '{'; \
-	echo '"Lineage": {'; \
-	echo '    "Has_legacy_camera_hal1": $(if $(filter true,$(TARGET_HAS_LEGACY_CAMERA_HAL1)),true,false),'; \
-	echo '    "Target_shim_libs": "$(subst $(space),:,$(TARGET_LD_SHIM_LIBS))"'; \
-	echo '},'; \
-	echo '') > $(SOONG_VARIABLES_TMP)
+_contents := $(_contents)    "Lineage":{$(newline)
+
+# See build/core/soong_config.mk for the add_json_* functions you can use here.
+$(call add_json_bool, Has_legacy_camera_hal1,                $(filter true,$(TARGET_HAS_LEGACY_CAMERA_HAL1)))
+$(call add_json_str,  Target_shim_libs,                      $(TARGET_LD_SHIM_LIBS))
+
+# This causes the build system to strip out the last comma in our nested struct, to keep the JSON valid.
+_contents := $(_contents)__SV_END
+
+_contents := $(_contents)    },$(newline)
