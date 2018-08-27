9113926fea0785dc30ce26e4a188b3404421b566
Bruno Martins <bgcngm@gmail.com>
2018-08-27 10:08:54 +0100

    soong: Remove extra spacing
     * Let's not rewrite the whole list while adding new vars
    
    Change-Id: Icab6116839bbb74a75d207310562faf9d780828b

--- build/soong/soong_config.mk
+++ build/soong/soong_config.mk
@@ -1,8 +1,8 @@
 _contents := $(_contents)    "Lineage":{$(newline)
 
 # See build/core/soong_config.mk for the add_json_* functions you can use here.
-$(call add_json_bool, Has_legacy_camera_hal1,                $(filter true,$(TARGET_HAS_LEGACY_CAMERA_HAL1)))
-$(call add_json_str,  Target_shim_libs,                      $(TARGET_LD_SHIM_LIBS))
+$(call add_json_bool, Has_legacy_camera_hal1, $(filter true,$(TARGET_HAS_LEGACY_CAMERA_HAL1)))
+$(call add_json_str, Target_shim_libs, $(TARGET_LD_SHIM_LIBS))
 
 # This causes the build system to strip out the last comma in our nested struct, to keep the JSON valid.
 _contents := $(_contents)__SV_END
