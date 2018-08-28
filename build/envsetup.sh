50872bc254896eea27bc1c3b46bc6641bba3b39a
YumeMichi <do4suki@gmail.com>
2018-08-28 20:53:25 +0800

    envsetup: Use otapackage instead of bacon
     * We don't implement bacon target so just use aosp otapackage target
    
    Signed-off-by: YumeMichi <do4suki@gmail.com>

--- build/envsetup.sh
+++ build/envsetup.sh
@@ -4,7 +4,7 @@ Additional AOSP functions:
 - breakfast:       Setup the build environment, but only list
                    devices we support.
 - brunch:          Sets up build environment using breakfast(),
-                   and then comiles using mka() against bacon target.
+                   and then comiles using mka() against otapackage target.
 - mka:             Builds using SCHED_BATCH on all processors.
 - pushboot:        Push a file from your OUT dir to your phone and
                    reboots it, using absolute path.
@@ -15,7 +15,7 @@ function brunch()
 {
     breakfast $*
     if [ $? -eq 0 ]; then
-        time mka bacon
+        time mka otapackage
     else
         echo "No such item in brunch menu. Try 'breakfast'"
         return 1
