6125a04ccfde4be6f6010d5b2407b28d02692b07
Paul Keith <javelinanddart@gmail.com>
2018-09-21 00:52:58 +0200

    soong: Use add_json_str_omitempty directive for TARGET_LD_SHIM_LIBS
    * The code is ifdef'ed out, so let's match that expecation here
    
    Change-Id: I59c8e276c3dd28c4f46222f66d626e48b2aa6551

--- build/soong/soong_config.mk
+++ build/soong/soong_config.mk
@@ -7,7 +7,7 @@ $(call add_json_str_omitempty, Additional_gralloc_10_usage_bits, $(TARGET_ADDITI
 $(call add_json_bool, Has_legacy_camera_hal1, $(filter true,$(TARGET_HAS_LEGACY_CAMERA_HAL1)))
 $(call add_json_bool, Needs_text_relocations, $(filter true,$(TARGET_NEEDS_PLATFORM_TEXT_RELOCATIONS)))
 $(call add_json_str_omitempty, Target_process_sdk_version_override, $(TARGET_PROCESS_SDK_VERSION_OVERRIDE))
-$(call add_json_str, Target_shim_libs, $(TARGET_LD_SHIM_LIBS))
+$(call add_json_str_omitempty, Target_shim_libs, $(TARGET_LD_SHIM_LIBS))
 
 # This causes the build system to strip out the last comma in our nested struct, to keep the JSON valid.
 _contents := $(_contents)__SV_END
