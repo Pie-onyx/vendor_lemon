PotatoProject/vendor_potato

soong_config: Switch to json_map calls
Change-Id: I8b18665e40bb12c532c119e7a59b5848a1f9fe75
Signed-off-by: Kshitij Gupta <kshitijgm@gmail.com>

build/soong/soong_config.mk
@@ -1,6 +1,5 @@
-_contents := $(_contents)    "Potato":{$(newline)
+$(call add_json_map, Potato)
 
-# See build/core/soong_config.mk for the add_json_* functions you can use here.
 $(call add_json_bool, Device_support_hwfde, $(filter true,$(TARGET_HW_DISK_ENCRYPTION)))
 $(call add_json_bool, Device_support_hwfde_perf, $(filter true,$(TARGET_HW_DISK_ENCRYPTION_PERF)))
 $(call add_json_bool, Device_support_legacy_hwfde, $(filter true,$(TARGET_LEGACY_HW_DISK_ENCRYPTION)))
@@ -13,7 +12,4 @@ $(call add_json_str, Target_shim_libs, $(TARGET_LD_SHIM_LIBS))
 $(call add_json_bool, Uses_generic_camera_parameter_library, $(if $(TARGET_SPECIFIC_CAMERA_PARAMETER_LIBRARY),false,true))
 $(call add_json_bool, Uses_qcom_bsp_legacy, $(filter true,$(TARGET_USES_QCOM_BSP_LEGACY)))
 
-# This causes the build system to strip out the last comma in our nested struct, to keep the JSON valid.
-_contents := $(_contents)__SV_END
-
-_contents := $(_contents)    },$(newline)
+$(call end_json_map)

Pie-onyx/vendor_lemon

build/soong/soong_config.mk
@@ -1,6 +1,6 @@
 add_json_str_omitempty = $(if $(strip $(2)),$(call add_json_str, $(1), $(2)))
 
-_contents := $(_contents)    "Lemon":{$(newline)
+$(call add_json_map, Lemon)
 
 # See build/core/soong_config.mk for the add_json_* functions you can use here.
 $(call add_json_str_omitempty, Additional_gralloc_10_usage_bits, $(TARGET_ADDITIONAL_GRALLOC_10_USAGE_BITS))
@@ -9,7 +9,4 @@ $(call add_json_bool, Needs_text_relocations, $(filter true,$(TARGET_NEEDS_PLATF
 $(call add_json_str_omitempty, Target_process_sdk_version_override, $(TARGET_PROCESS_SDK_VERSION_OVERRIDE))
 $(call add_json_str_omitempty, Target_shim_libs, $(TARGET_LD_SHIM_LIBS))
 
-# This causes the build system to strip out the last comma in our nested struct, to keep the JSON valid.
-_contents := $(_contents)__SV_END
-
-_contents := $(_contents)    },$(newline)
+$(call end_json_map)