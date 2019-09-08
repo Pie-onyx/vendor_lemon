b276d194f2a1adb4611774dbc4fe050b9e74d78e
YumeMichi <do4suki@gmail.com>
2019-09-08 21:21:50 +0800

    vendor: Enable ART debug
    Signed-off-by: YumeMichi <do4suki@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -98,7 +98,7 @@ PRODUCT_BOOT_JARS += \
     telephony-ext
 
 # Do not include art debug targets
-PRODUCT_ART_TARGET_INCLUDE_DEBUG_BUILD := false
+# PRODUCT_ART_TARGET_INCLUDE_DEBUG_BUILD := false
 
 # Strip the local variable table and the local variable type table to reduce
 # the size of the system image. This has no bearing on stack traces, but will
