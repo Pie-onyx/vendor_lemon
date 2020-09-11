c4ea4a92c56d1ca0020b320e7667882b2ebab119
Jan Altensen <info@stricted.net>
2020-09-11 21:41:08 +0200

    bacon: switch to toybox md5sum
     * fixes "error: MD5SUM is obsolete."
    
    Change-Id: I38e167e24e9eb5596a1e8e2f2d915f648bc1283c

--- build/tasks/bacon.mk
+++ build/tasks/bacon.mk
@@ -6,8 +6,10 @@ endif
 
 LEMON_TARGET_PACKAGE := $(PRODUCT_OUT)/lemon-$(PLATFORM_VERSION)-$(shell date +%Y%m%d)-$(TARGET_DEVICE)-$(LEMON_BUILD_ID).zip
 
+MD5 := prebuilts/build-tools/path/$(HOST_OS)-x86/md5sum
+
 .PHONY: bacon
 bacon: $(INTERNAL_OTA_PACKAGE_TARGET)
 	$(hide) ln -f $(INTERNAL_OTA_PACKAGE_TARGET) $(LEMON_TARGET_PACKAGE)
-	$(hide) $(MD5SUM) $(LEMON_TARGET_PACKAGE) | sed "s|$(PRODUCT_OUT)/||" > $(LEMON_TARGET_PACKAGE).md5sum
+	$(hide) $(MD5) $(LEMON_TARGET_PACKAGE) | sed "s|$(PRODUCT_OUT)/||" > $(LEMON_TARGET_PACKAGE).md5sum
 	@echo "Package Complete: $(LEMON_TARGET_PACKAGE)" >&2
