e237950993d322506487a25aaba38221f294d020
YumeMichi <do4suki@gmail.com>
2020-09-20 14:59:40 +0800

    kernel: Squashed reverts non-lineage changes
    Revert "header-gen: Use MAKE_PREBUILT and expose origpath to soong"
    Revert "kernel: Workaround PHONY dependencies issues"
    Revert "kernel: Use prebuilt make and load origpath"
    Revert "soong_config: Switch to json_map calls"
    
    Change-Id: Iff53a73b2bec3edfe1c03f50c0ce1055117c604d
    Signed-off-by: YumeMichi <do4suki@gmail.com>

--- build/soong/soong_config.mk
+++ build/soong/soong_config.mk
@@ -1,6 +1,6 @@
 add_json_str_omitempty = $(if $(strip $(2)),$(call add_json_str, $(1), $(2)))
 
-$(call add_json_map, Lemon)
+_contents := $(_contents)    "Lemon":{$(newline)
 
 # See build/core/soong_config.mk for the add_json_* functions you can use here.
 $(call add_json_str_omitempty, Additional_gralloc_10_usage_bits, $(TARGET_ADDITIONAL_GRALLOC_10_USAGE_BITS))
@@ -10,4 +10,7 @@ $(call add_json_str_omitempty, Target_init_vendor_lib, $(TARGET_INIT_VENDOR_LIB)
 $(call add_json_str_omitempty, Target_process_sdk_version_override, $(TARGET_PROCESS_SDK_VERSION_OVERRIDE))
 $(call add_json_str_omitempty, Target_shim_libs, $(subst $(space),:,$(TARGET_LD_SHIM_LIBS)))
 
-$(call end_json_map)
+# This causes the build system to strip out the last comma in our nested struct, to keep the JSON valid.
+_contents := $(_contents)__SV_END
+
+_contents := $(_contents)    },$(newline)
--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -1,13 +1,10 @@
 # Add variables that we wish to make available to soong here.
-ORIG_PATH := $(shell cat $(OUT_DIR)/.path_interposer_origpath)
 EXPORT_TO_SOONG := \
     KERNEL_ARCH \
     KERNEL_CROSS_COMPILE \
     KERNEL_MAKE_FLAGS \
     TARGET_KERNEL_CONFIG \
-    TARGET_KERNEL_SOURCE \
-    MAKE_PREBUILT \
-    ORIG_PATH
+    TARGET_KERNEL_SOURCE
 
 # Setup SOONG_CONFIG_* vars to export the vars listed above.
 # Documentation here:
