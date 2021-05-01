f729146b677bb59f3b865c56bd6f986a629dd298
Julian Veit <Claymore1298@gmail.com>
2021-05-01 11:56:15 +0200

    bacon: switch to sha256 checksum
    As md5 is not considered as secure since some time, and sha256 is
    industry standard, lets switch over here too.
    
    Change-Id: I22015be20126a4348affa5ef97e2c28fbd7b4717

--- build/tasks/bacon.mk
+++ build/tasks/bacon.mk
@@ -6,10 +6,10 @@ endif
 
 LEMON_TARGET_PACKAGE := $(PRODUCT_OUT)/lemon-$(PLATFORM_VERSION)-$(shell date +%Y%m%d)-$(TARGET_DEVICE)-$(LEMON_BUILD_ID).zip
 
-MD5 := prebuilts/build-tools/path/$(HOST_PREBUILT_TAG)/md5sum
+SHA256 := prebuilts/build-tools/path/$(HOST_PREBUILT_TAG)/sha256sum
 
 .PHONY: bacon
 bacon: $(INTERNAL_OTA_PACKAGE_TARGET)
 	$(hide) ln -f $(INTERNAL_OTA_PACKAGE_TARGET) $(LEMON_TARGET_PACKAGE)
-	$(hide) $(MD5) $(LEMON_TARGET_PACKAGE) | sed "s|$(PRODUCT_OUT)/||" > $(LEMON_TARGET_PACKAGE).md5sum
+	$(hide) $(SHA256) $(LEMON_TARGET_PACKAGE) | sed "s|$(PRODUCT_OUT)/||" > $(LEMON_TARGET_PACKAGE).sha256sum
 	@echo "Package Complete: $(LEMON_TARGET_PACKAGE)" >&2
